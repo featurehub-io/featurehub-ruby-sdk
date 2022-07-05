e4cdf985201ef85fb715fb5508bada1263c2d3a5
Chris Spalding <cspalding93@gmail.com>
2022-07-04 22:13:55 -0700

    add dockerfile and featurehub url when running via docker

--- examples/rails_example/config/initializers/featurehub.rb
+++ examples/rails_example/config/initializers/featurehub.rb
@@ -5,6 +5,8 @@ Rails.configuration.fh_config = FeatureHub::Sdk::FeatureHubConfig.new(
     # you may have to change this URL in your app. Assuming you're running the party server locally 
     # (i.e. what's described here https://www.featurehub.io/) to test out FeatureHub you can find this URL by running
     # docker ps and finding the container with an image name like 'featurehub/party-server:latest'
+    # running via docker? use this URL:
+    # "http://host.docker.internal:8085/",
     "http://0.0.0.0:8085/",
     # You can find this value in the admin console by navigating to the 'API Keys' section of the UI and choosing
     # between the client and the server API Keys for whatever service account you've set up. Learn more about the
